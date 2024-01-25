d8859d091a7d6924972ffc30e43a520eff908455
Luiz Rocco <luizgustavorocco@gmail.com>
2024-01-25 01:15:15 -0300

    enable drizzle relational queries

--- db/db-client.ts
+++ db/db-client.ts
@@ -1,8 +1,8 @@
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
-import { todos } from "./schema";
+import * as schema from "./schema";
 
 const sqlite = new Database("./db/sqlite.db");
-const db = drizzle(sqlite);
+const db = drizzle(sqlite, { schema });
 
 export default db;
